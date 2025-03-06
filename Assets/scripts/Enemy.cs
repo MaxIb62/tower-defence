2be8dbc471a5bc9075ba49a9472ca43604a39fdd
Max Ibarra <cdmx2254@amerike.edu.mx>
2025-03-06 16:20:53 -0600

    proyect

---
+++ Assets/scripts/Enemy.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex;
+
+    void Start()
+    {
+        target = waypoints.points[0];
+    }
+
+    void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position )<= 0.2f)
+        {
+            GetNextWayPoint();
+        }
+    }
+
+    void GetNextWayPoint()
+    {
+        if (wavepointIndex >= waypoints.points.Length -1)
+        {
+            Destroy(gameObject);
+        }
+        wavepointIndex++;
+        target = waypoints.points[wavepointIndex];
+    }
+}
